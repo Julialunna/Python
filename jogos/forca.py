a60cfeaa205b03d4b9bcf89bef84f23b0fe9801a
Júlia Luna <85693548+Julialunna@users.noreply.github.com>
2022-12-15 20:47:16 -0300

    terminando jogo

---
+++ jogos/forca.py
@@ -0,0 +1,9 @@
+def jogo_forca():
+
+        print("************************************")
+        print("***Bem vindo(a) ao jogo de Forca!***")
+        print("************************************")
+        print("Fim do jogo")
+        
+if(__name__=="__main__"):
+    jogo_forca()
