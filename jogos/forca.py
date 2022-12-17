ed14feb5f8c1cceeadca3223beeb28190d981edb
Júlia Luna <85693548+Julialunna@users.noreply.github.com>
2022-12-17 14:16:10 -0300

    fazendo jogo de forca

--- jogos/forca.py
+++ jogos/forca.py
@@ -4,6 +4,15 @@ def jogo_forca():
         print("***Bem vindo(a) ao jogo de Forca!***")
         print("************************************")
         print("Fim do jogo")
-        
+
+        palavra_secreta = "banana"
+
+        enforcou = False
+        acertou = False 
+
+        while(not enforcou and not acertou):
+            print("jogando")
+            
+
 if(__name__=="__main__"):
     jogo_forca()
